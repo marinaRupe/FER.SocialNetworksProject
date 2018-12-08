86746763643935d4878b5e2e3759ccfe712e2343
Marina Rupe <marina.rupe6@gmail.com>
2018-12-08 22:01:20 +0100

    Add updating movie on save (if movie already exists)

--- node/services/movie.service.js
+++ node/services/movie.service.js
@@ -16,6 +16,10 @@ const saveMovie = async movie => {
   await newMovie.save();
 };
 
+const updateMovie = async movie => {
+  await Movie.findByIdAndUpdate(movie._id, { $set: movie });
+};
+
 const saveMovieList = async movies => {
   console.info('Saving movies...');
 
@@ -26,6 +30,14 @@ const saveMovieList = async movies => {
         console.info(`The movie with imdb ID ${movie.imdbID} is saved (tmdbID: ${movie.tmdbID})!`);
       } catch (err) {
         console.info(`The movie with imdb ID ${movie.imdbID} is NOT saved (tmdbID: ${movie.tmdbID})!`);
+        console.info(err.message);
+      }
+    } else {
+      try {
+        await updateMovie(movie);
+        console.info(`The movie with imdb ID ${movie.imdbID} is updated (tmdbID: ${movie.tmdbID})!`);
+      } catch (err) {
+        console.info(`The movie with imdb ID ${movie.imdbID} is NOT updated (tmdbID: ${movie.tmdbID})!`);
         console.info(err.message);
       }
     }
